0a9ea3c604c4e555b19b54e80c9c11bb335a4bf5
Ulugbek <90781475+ulugbekxyz@users.noreply.github.com>
2024-12-28 04:02:42 +0500

    Update settings.py
    удалил ключ

--- nextstep_backend/nextstep_backend/settings.py
+++ nextstep_backend/nextstep_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_$sf&4ywxv0^^4ikqj2%1xt$@36rlw24xm)^3mi==c&i_g3ix%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
